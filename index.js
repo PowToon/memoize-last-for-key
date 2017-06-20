e80113e226f33c585f32cf434cd2efbaf2a806ec
vzaidman <vzaidman@gmail.com>
2017-06-20 10:39:03 +0300

    empty size will cause no cache clearing

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = function keyArgsMemoize(fn, options){
       })
     }
 
-    if(shouldAddKey){
+    if(size && shouldAddKey){
       cache.removeOldKeys(size, entryOrder)
     }
 
